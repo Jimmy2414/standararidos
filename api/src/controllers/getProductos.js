5b25390f794743d6504a9b79cc6eeecebde900a2
Jimmy2414 <96069053+Jimmy2414@users.noreply.github.com>
2022-08-17 13:41:10 -0300

    Update getProductos.js

--- api/src/controllers/getProductos.js
+++ api/src/controllers/getProductos.js
@@ -1,11 +1,12 @@
 'use Strict';
 const { Productos } = require('../db.js');
+const { Op } = require('sequelize');
 
 async function getProductByName(req, res) {
-  const { nombre } = req.params;
+  const nombre = `%${req.params.nombre}%`;
   const ProductosName = await Productos.findAll({
     where: {
-      nombre,
+      nombre: { [Op.like]: nombre.toLocaleLowerCase() },
     },
   });
   res.send(ProductosName);
