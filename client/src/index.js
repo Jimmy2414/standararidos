1bb9ec101b252034d82e1eacc55a008496d06291
Jimmy2414 <96069053+Jimmy2414@users.noreply.github.com>
2022-08-23 19:48:12 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import store from '../src/Redux/store/store';
-
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
