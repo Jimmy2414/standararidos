6cafbf18cb7d603179e66a375dbecfeeee28c252
lucascencig <cenciglucas@gmail.com>
2022-08-24 13:54:36 -0300

    RESPONSIVE CASI FINAL

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -16,35 +16,6 @@ export default function Footer() {
           </ul>
         </div>
 
-        <div className={s.inicio}>
-          {/* <p>Somos una marca joven con el objetivo de desarrollar productos para todo aquel que quiera remodelar y acondicionar su hogar, a su manera. <br />
-            Nos dedicamos a fabricar y comercializar, productos para la construcción que sirvan para todo tipo de necesidades, y además brindar un aspecto decorativo característico al tanto de las tendencias emergentes. Ejemplo de ello, son los revestimientos texturados, un producto intrínseco a nuestra marca. <br />
-            Constantemente, buscamos mejorar nuestras fórmulas de manera que se adecúen aún más a las expectativas de nuestros clientes. <br />
-            Apuntamos a lograr, día a día, un producto de suma calidad y resistencia. </p> */}
-          {/* <h2>Inicio</h2>
-          <ul>
-            <NavLink to={'/revestimientos-texturados'}>
-              <li>Revestimientos Texturados</li>
-            </NavLink>
-
-            <NavLink to={'/latex-color'}>
-              <li>Látex color</li>
-            </NavLink>
-
-            <NavLink to={'/membranas'}>
-              <li>Membranas</li>
-            </NavLink>
-
-            <NavLink to={'/preparacion-de-la-superficie'}>
-              <li>Preparación de la superficie</li>
-            </NavLink>
-
-            <NavLink to={'/auxilires'}>
-              <li>Auxiliares</li>
-            </NavLink>
-          </ul> */}
-        </div>
-
         <div className={s.redes}>
           <h2>Seguinos en</h2>
           <ul>
@@ -58,12 +29,7 @@ export default function Footer() {
           </ul>
         </div>
 
-        {/* <div className={s.titulo}>
-          <span><p className={s.quienessomos}>¿QUIENES SOMOS?</p>  Somos una marca joven con el objetivo de desarrollar productos para todo aquel que quiera remodelar y acondicionar su hogar, a su manera. <br />
-            Nos dedicamos a fabricar y comercializar, productos para la construcción que sirvan para todo tipo de necesidades, y además brindar un aspecto decorativo característico al tanto de las tendencias emergentes. Ejemplo de ello, son los revestimientos texturados, un producto intrínseco a nuestra marca. <br />
-            Constantemente, buscamos mejorar nuestras fórmulas de manera que se adecúen aún más a las expectativas de nuestros clientes. <br />
-            Apuntamos a lograr, día a día, un producto de suma calidad y resistencia. </span>
-        </div> */}
+
       </div>
 
       <div className={s.infoFinal}>
